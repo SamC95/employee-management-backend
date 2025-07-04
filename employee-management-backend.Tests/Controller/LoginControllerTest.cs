883d3d0034abefdece419762264d10f7241bc198
SamC95 <132593571+sJagstang@users.noreply.github.com>
2025-07-04 11:51:37 +0100

    test: implemented test cases for login validation

---
+++ employee-management-backend.Tests/Controller/LoginControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using employee_management_backend.Controller;
+using employee_management_backend.Model;
+using employee_management_backend.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace employee_management_backend.Tests.Controller;
+
+public class LoginControllerTest
+{
+    private readonly Mock<ILoginService> _mockService;
+    private readonly LoginController _controller;
+
+    public LoginControllerTest()
+    {
+        _mockService = new Mock<ILoginService>();
+        _controller = new LoginController(_mockService.Object);
+    }
+
+    private readonly LoginDetails _testLogin = new()
+    {
+        UserId = "7734021",
+        Password = "testpassword",
+    };
+
+    [Fact]
+    public async Task ValidateLogin_WhenSuccessful_ReturnsOk()
+    {
+        var result = await _controller.ValidateLogin(_testLogin);
+        
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(200, okResult.StatusCode);
+        Assert.NotNull(okResult.Value);
+        
+        _mockService.Verify(service => service.ValidateLogin(_testLogin), Times.Once);
+    }
+
+    [Fact]
+    public async Task ValidateLogin_WhenUnhandledExceptionThrown_ReturnsInternalServerError()
+    {
+        _mockService.Setup(service => service.ValidateLogin(_testLogin)).Throws(new Exception());
+        
+        var result = await _controller.ValidateLogin(_testLogin);
+        
+        var objectResult = Assert.IsType<ObjectResult>(result);
+        Assert.Equal(500, objectResult.StatusCode);
+        Assert.NotNull(objectResult.Value);
+        
+        _mockService.Verify(service => service.ValidateLogin(_testLogin), Times.Once);
+    }
+}
---
+++ employee-management-backend.Tests/Service/LoginServiceTest.cs
@@ -0,0 +1,119 @@
+﻿using employee_management_backend.Model;
+using employee_management_backend.Repository.Interface;
+using employee_management_backend.Service;
+using employee_management_backend.Service.Utils.Passwords;
+using Moq;
+
+namespace employee_management_backend.Tests.Service;
+
+public class LoginServiceTest
+{
+    private readonly Mock<IEmployeeRepository> _mockEmployeeRepository;
+    private readonly LoginService _service;
+
+    public LoginServiceTest()
+    {
+        _mockEmployeeRepository = new Mock<IEmployeeRepository>();
+        _service = new LoginService(_mockEmployeeRepository.Object);
+    }
+
+    private readonly LoginDetails _testLogin = new()
+    {
+        UserId = "7734021",
+        Password = "testpassword",
+    };
+
+    private readonly Employee _testEmployee = new()
+    {
+        EmployeeId = "7734021",
+        ClockId = "9988776",
+        FirstName = "Hassan",
+        LastName = "Patel",
+        Email = "hassan.patel@vertexinnovations.co.uk",
+        PhoneNum = "07700 900234",
+        Address = "42 Tech Park Avenue",
+        City = "Manchester",
+        PostCode = "M1 4AB",
+        Country = "United Kingdom",
+        Gender = "Male",
+        DateOfBirth = new DateOnly(1990,
+            3,
+            14),
+        DateHired = new DateOnly(2022,
+            6,
+            1),
+        IsAdmin = false,
+        IsManager = true,
+        IsActive = true,
+        JobTitle = "Product Developer",
+        NationalInsuranceNumber = "BB1234567",
+        NationalInsuranceCategory = "A",
+        TaxCode = "1257L",
+        HasPension = false,
+        EmployeePensionContributionPercentage = 0,
+        EmployerPensionContributionPercentage = 0,
+        HasUnion = false,
+        UnionContributionPercentage = 0,
+        PayPerHour = 23.40m
+    };
+
+    [Fact]
+    public async Task ValidateLogin_ShouldReturnTrue_WhenUserIdAndPasswordAreCorrect()
+    {
+        var hashedPassword = HashPasswordUtil.PerformPasswordHash("testpassword");
+
+        _testEmployee.Password = hashedPassword;
+
+        _mockEmployeeRepository.Setup(repository => repository.GetEmployeeById(_testEmployee.EmployeeId))
+            .ReturnsAsync(_testEmployee);
+
+        var result = await _service.ValidateLogin(_testLogin);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task ValidateLogin_ShouldReturnFalse_WhenUserDoesNotExist()
+    {
+        _mockEmployeeRepository.Setup(repository => repository.GetEmployeeById("12345678"))
+            .ReturnsAsync((Employee)null!);
+
+        var result = await _service.ValidateLogin(_testLogin);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ValidateLogin_ShouldReturnFalse_WhenPasswordIsIncorrect()
+    {
+        var hashedPassword = HashPasswordUtil.PerformPasswordHash("correctpassword");
+
+        _testEmployee.Password = hashedPassword;
+
+        _mockEmployeeRepository.Setup(repository => repository.GetEmployeeById(_testEmployee.EmployeeId))
+            .ReturnsAsync(_testEmployee);
+
+        var result = await _service.ValidateLogin(_testLogin);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ValidateLogin_ShouldReturnFalse_WhenEmployeePasswordIsNullOrEmpty()
+    {
+        _testEmployee.Password = null;
+
+        _mockEmployeeRepository.Setup(repository => repository.GetEmployeeById(_testEmployee.EmployeeId))
+            .ReturnsAsync(_testEmployee);
+        
+        var result = await _service.ValidateLogin(_testLogin);
+        
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ValidateLogin_ShouldThrowArgumentNullException_WhenLoginDetailsAreNull()
+    {
+        await Assert.ThrowsAsync<ArgumentNullException>(() => _service.ValidateLogin(null!));
+    }
+}
